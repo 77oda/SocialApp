a4893eb96a3f6561a129f2e32578a8f6e7316f39
kordy21 <m.abdelhamed941@gmail.com>
2023-09-23 18:35:47 +0300

    v9

--- lib/modules/social_app/feeds/feeds_screen.dart
+++ lib/modules/social_app/feeds/feeds_screen.dart
@@ -31,7 +31,8 @@ class FeedsScreen extends StatelessWidget
           },
           child:ConditionalBuilder(
             condition: SocialCubit.get(context).posts.isNotEmpty && state is! SocialGetStoryLoadingState ,
-            builder: (context) => SingleChildScrollView(
+            builder: (context) =>
+                SingleChildScrollView(
               physics: const BouncingScrollPhysics(),
               child: Column(
                 children:
